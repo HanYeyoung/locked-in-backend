fd7617d3cbd3149cb47c29de331a9fb75e835ec9
Tina Li <tli442@wisc.edu>
2024-10-18 09:24:30 -0500

    add remove_text method

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import random
 from scipy.ndimage import distance_transform_edt
 from skimage.morphology import remove_small_objects
 from skimage.segmentation import watershed, clear_border
+import keras_ocr
+import math
 
 
 def remove_gaps(image_path):
@@ -130,8 +132,49 @@ def identify_rooms(image_path):
 
     return result
 
+def inpaint_text(img_path, pipeline):
+    # read the image
+    img = keras_ocr.tools.read(img_path)
+
+    # Recogize text (and corresponding regions)
+    # Each list of predictions in prediction_groups is a list of
+    # (word, box) tuples.
+    prediction_groups = pipeline.recognize([img])
+
+    #Define the mask for inpainting
+    mask = np.zeros(img.shape[:2], dtype="uint8")
+    for box in prediction_groups[0]:
+        x0, y0 = box[1][0]
+        x1, y1 = box[1][1]
+        x2, y2 = box[1][2]
+        x3, y3 = box[1][3]
+
+        x_mid0, y_mid0 = midpoint(x1, y1, x2, y2)
+        x_mid1, y_mi1 = midpoint(x0, y0, x3, y3)
+
+        #For the line thickness, we will calculate the length of the line between
+        #the top-left corner and the bottom-left corner.
+        thickness = int(math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 ))
+
+        #Define the line and inpaint
+        cv2.line(mask, (x_mid0, y_mid0), (x_mid1, y_mi1), 255,
+                 thickness)
+        inpainted_img = cv2.inpaint(img, mask, 7, cv2.INPAINT_NS)
+
+    return(inpainted_img)
+
+def midpoint(x1, y1, x2, y2):
+    x_mid = int((x1 + x2)/2)
+    y_mid = int((y1 + y2)/2)
+    return (x_mid, y_mid)
+
 # Usage
 if __name__ == "__main__":
+    pipeline = keras_ocr.pipeline.Pipeline()
+    img_text_removed = inpaint_text('floorplans/test/hard.jpg', pipeline)
+    plt.imshow(img_text_removed)
+    cv2.imwrite('text_removed_image.jpg', cv2.cvtColor(img_text_removed, cv2.COLOR_BGR2RGB))
+
     image = remove_gaps("floorplans/test/empty.jpg")
     plt.figure(figsize=(10, 10))
     plt.imshow(image)
